ee55b8d2ed9945c3210a6dea99fcf020a0c63a1d
Richard Bultitude <rbultitude@zonedigital.com>
2017-03-06 18:00:57 +0000

    create index for node and point to lib

---
+++ index.js
@@ -0,0 +1,7 @@
+//
+// Dark Sky JS module index
+//
+// Copyright (c) 2017 Richard Bultitude. See LICENSE-MIT for further details.
+//
+// @see README.md for usage information
+module.exports = require('./lib/darkskyjs');
