e8d9daca3d8907cafb9a3428979ac19549a0144b
rjbultitude <richard.bultitude@gmail.com>
2017-07-29 16:43:08 +0100

    change reporter. write better check for service data. add tests for new methods

--- test/darksky.spec.js
+++ test/darksky.spec.js
@@ -36,11 +36,13 @@ describe('Test the methods of DarkSky', function() {
         this.xhr = sinon.useFakeXMLHttpRequest();
         this.xhr.response = {};
         this.makeRequestStub = sinon.stub(this.darkSky, 'makeRequest');
+        sinon.spy(this.darkSky, 'processData');
     });
 
     afterEach(function() {
         this.makeRequestStub.restore();
         this.xhr.restore();
+        this.darkSky.processData.restore();
     });
 
     //create scale spec
@@ -81,8 +83,22 @@ describe('Test the methods of DarkSky', function() {
     it('requestAllLocData should throw an error when passed a non-iterable data type', function() {
         expect(function () {this.darkSky.requestAllLocData('test')}).to.throw();
     });
-    xit('getCurrentConditions should call the callback on all success', function() {
+    it('checkData should return false when forecasts arg has length of 0', function() {
+        expect(this.darkSky.checkData([])).to.be.false;
+    });
+    it('checkData should return false when forecasts arg is null', function() {
+        expect(this.darkSky.checkData(null)).to.be.false;
+    });
+    it('checkData should return false when forecasts arg is undefined', function() {
+        expect(this.darkSky.checkData(undefined)).to.be.false;
+    });
+    it('processData should be called once when checkData is called with correct args', function() {
+        this.darkSky.checkData(['test']);
+        expect(this.darkSky.processData).to.be.calledOnce;
+    });
+    it('processData should call it\'s callback arg', function() {
         var callback = sinon.spy();
-        expect(this.getCurrentConditions([0, 1, 2], callback)).to.call(callback);
+        this.darkSky.processData([0, 1, 2], [0, 1, 2], 'processCurrent', callback);
+        expect(callback).to.be.calledOnce;
     });
 });
